12bc771be623add1a82f02b3be17286771065231
Javier Palacios <javiplx@gmail.com>
2010-07-03 16:00:04 +0200

    BUGFIX : priority is not a required key

--- repolib/debian.py
+++ repolib/debian.py
@@ -220,9 +220,9 @@ class debian_repository ( abstract_repository ) :
     def match_filters( self , pkginfo , filters ) :
         if filters.has_key('sections') and pkginfo['Section'] not in filters['sections'] :
             return False
-        if filters.has_key('priorities') and pkginfo['Priority'] not in filters['priorities'] :
+        if filters.has_key('priorities') and pkginfo.has_key('Priority') and pkginfo['Priority'] not in filters['priorities'] :
             return False
-        if filters.has_key('tags') and 'Tag' in pkginfo.keys() and pkginfo['Tag'] not in filters['tags'] :
+        if filters.has_key('tags') and pkginfo.has_key('Tag') and pkginfo['Tag'] not in filters['tags'] :
             return False
         return True
 
