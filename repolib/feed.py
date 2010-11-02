ee4bd54de8c83a0fbbc0098ecb12a00af4150cc2
Javier Palacios <javiplx@gmail.com>
2010-11-02 11:53:07 +0100

    Allow multiple extensions for feed repositories

--- repolib/feed.py
+++ repolib/feed.py
@@ -13,13 +13,15 @@ class feed_build_repository ( abstract_build_repository ) :
 
         abstract_build_repository.__init__( self , config )
 
+        self.valid_extensions = ( ".opk" , ".ipk" )
+
         self.components = config.get( "components" , None )
 
     def build ( self ) :
 
         packages = open( os.path.join( self.destdir , "Packages" ) , 'w' )
 
-        for filename in filter( lambda x : x.endswith( ".opk" ) , os.listdir( self.destdir ) ) :
+        for filename in filter( lambda x : os.path.splitext(x)[1] in self.valid_extensions , os.listdir( self.destdir ) ) :
             pkg = debian_bundle.debfile.DebFile( os.path.join( self.destdir , filename ) )
             control = pkg.control.debcontrol()
             if not control.has_key("Filename") :
