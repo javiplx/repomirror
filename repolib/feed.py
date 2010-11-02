56ad62552f3dd869f8fdd2674cdffb810b2fd3f1
Javier Palacios <javiplx@gmail.com>
2010-11-02 11:44:47 +0100

    BUGFIX : Add filename to packages description file

--- repolib/feed.py
+++ repolib/feed.py
@@ -21,7 +21,10 @@ class feed_build_repository ( abstract_build_repository ) :
 
         for filename in filter( lambda x : x.endswith( ".opk" ) , os.listdir( self.destdir ) ) :
             pkg = debian_bundle.debfile.DebFile( os.path.join( self.destdir , filename ) )
-            packages.write( "%s\n" % pkg.control.debcontrol() )
+            control = pkg.control.debcontrol()
+            if not control.has_key("Filename") :
+                control["Filename"] = filename
+            packages.write( "%s\n" % control )
 
         packages.close()
 
