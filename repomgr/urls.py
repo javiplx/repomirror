943252bee52760bd631a799f215fc94bfdd271a1
Javier Palacios <javiplx@gmail.com>
2010-12-15 21:05:24 +0100

    Implement detailed repo view

--- repomgr/urls.py
+++ repomgr/urls.py
@@ -22,7 +22,7 @@ def index ( request ) :
             response.write( "<li><b>%s</b> - %s\n" % item )
         response.write( "</ul>\n" )
     for section in sections :
-        response.write( "<h3>%s</h3>\n" % section )
+        response.write( "<h3><a href=%s>%s</a></h3>\n" % ( section , section ) )
         response.write( "<ul>\n" )
         for item in config.items( section ) :
             response.write( "<li><b>%s</b> - %s\n" % item )
@@ -31,7 +31,33 @@ def index ( request ) :
     return response
 
 
+def detail ( request , repo_name ) :
+
+    response = HttpResponse()
+    config = ConfigParser.RawConfigParser()
+    if not config.read( [ "/etc/repomirror.conf" , os.path.expanduser("~/.repomirror") ] ) :
+        response.write( "Server Error\n" )
+        return response
+    if not config.has_section( repo_name ) :
+        response.write( "Repository '%s' does not exists\n" % repo_name )
+        return response
+    _global = []
+    if config.has_section( "global" ) :
+        _global = map( lambda x : "%s:%s" % x , config.items('global') )
+    repo = map( lambda x : "%s:%s" % x , config.items(repo_name) )
+    response.write( "<h3>%s</h3>\n" % repo_name )
+    response.write( "<ul>\n" )
+    for item in config.items( repo_name ) :
+        response.write( "<li><b>%s</b> - %s\n" % item )
+    response.write( "</ul>\n" )
+
+    response.write( "<a href=./>Go to main page</a>\n" )
+
+    return response
+
+
 urlpatterns = patterns('',
-    (r'^.*$', index),
+    (r'^$', index),
+    (r'^(?P<repo_name>.+)$', detail )
 )
 
