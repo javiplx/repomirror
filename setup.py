7104259edc5ed6fd1c58e2e9acddb38c57ae8dda
Javier Palacios <javiplx@gmail.com>
2011-01-03 00:37:56 +0100

    Add new files to manifest and distutils, upgrading version

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from distutils.core import setup
 
 setup(
     name = 'repomirror',
-    version = '1.3',
+    version = '1.3.3',
     description = 'Generic repository mirroring tool',
     author = 'Javier Palacios',
     author_email = 'javiplx@gmail.com',
     license = 'GPLv2',
     url = 'http://github.com/javiplx/repomirror',
     download_url = 'http://r26936.ovh.net/repomirror',
-    scripts = [ 'repomirror' , 'get_filters' , 'buildrepo' ],
+    scripts = [ 'repomirror' , 'get_filters' , 'buildrepo' , 'checkmirror' ],
     data_files = [
-                 ( 'share/repomirror' , [ 'repomirror.conf' , 'buildrepo.conf' ] )
+                 ( 'share/repomirror' , [ 'repomirror.conf' , 'buildrepo.conf' ] ) ,
+                 ( 'share/repomgr' , [ 'repomgr/repomgr.conf' ] )
                  ],
     packages = [ 'repolib' , 'repomgr' ],
     py_modules = [ 'filelist_xmlparser' ]
